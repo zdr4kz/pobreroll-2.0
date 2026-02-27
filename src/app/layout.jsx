2745e5f957b21201da29feaaa777f9641428b4ad
Zer0o0 <henrique.cosmemk4@gmail.com>
2026-02-27 17:19:12 -0300

    a

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,7 +8,7 @@ import { useEffect, useState, Suspense } from "react";
 import { usePathname } from "next/navigation"; // ✅ IMPORTAÇÃO CORRETA
 
 // Componentes
-import Login from "@/app/Login/page";
+import Login from "@/app/login/page";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
